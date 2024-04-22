a9c7861db1aba3fc301f207f027ca48a829d9985
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2024-04-22 20:23:51 +0200

    Tablet asset mm.

--- Assets/Marius/Scripts/FixedWingScripts/FixedWing_Controller.cs
+++ Assets/Marius/Scripts/FixedWingScripts/FixedWing_Controller.cs
@@ -3,105 +3,110 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof (AircraftPhysics))]
-    [RequireComponent(typeof(FixedWing_Inputs))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AircraftPhysics))]
+[RequireComponent(typeof(FixedWing_Inputs))]
 
-    public class FixedWing_Controller : MonoBehaviour
+public class FixedWing_Controller : MonoBehaviour
+{
+    [SerializeField] List<AeroSurface> controlSurfaces = null;
+    [SerializeField] List<WheelCollider> wheels = null;
+
+    [SerializeField] float rollControlSensitivity = 0.2f;
+    [SerializeField] float pitchControlSensitivity = 0.2f;
+    [SerializeField] float yawControlSensitivity = 0.2f;
+
+    [Range(0, 1)] public float Flap;
+
+    [SerializeField] Text displayText = null;
+
+    [SerializeField] private GameObject propeller;
+    [SerializeField] private float propellerSpeed = 500f;
+
+    private float thrustPercent;
+
+    private float brakesTorque;
+
+    private AircraftPhysics aircraftPhysics;
+    private Rigidbody rb;
+    private FixedWing_Inputs FWInputs;
+
+    private void Start()
     {
-        [SerializeField] List<AeroSurface> controlSurfaces = null;
-        [SerializeField] List<WheelCollider> wheels = null;
-
-        [SerializeField] float rollControlSensitivity = 0.2f;
-        [SerializeField] float pitchControlSensitivity = 0.2f;
-        [SerializeField] float yawControlSensitivity = 0.2f;
-
-        [Range(0, 1)] public float Flap;
-
-        [SerializeField] Text displayText = null;
-
-        [Range(0, 100)] float thrustPercent;
-
-        float brakesTorque;
-
-        private AircraftPhysics aircraftPhysics;
-        private Rigidbody rb;
-        private FixedWing_Inputs FWInputs;
-
-        private void Start()
-        {
-            aircraftPhysics = GetComponent<AircraftPhysics>();
-            rb = GetComponent<Rigidbody>();
-            FWInputs = GetComponent<FixedWing_Inputs>();
-        }
-
-        private void Update()
-        {
-            /*if (Input.GetKeyDown(KeyCode.Space))
-            {
-                thrustPercent = FWInputs.Throttle;
-                //thrustPercent = thrustPercent > 0 ? 0 : 1f;
-            }*/
-
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                Flap = Flap > 0 ? 0 : 0.3f;
-            }
-
-            if (Input.GetKeyDown(KeyCode.B))
-            {
-                brakesTorque = brakesTorque > 0 ? 0 : 100f;
-            }
-
-            displayText.text = "V: " + ((int)rb.velocity.magnitude).ToString("D3") + " m/s\n";
-            displayText.text += "A: " + ((int)transform.position.y).ToString("D4") + " m\n";
-            displayText.text += "T: " + (int)(thrustPercent * 100) + "%\n";
-            displayText.text += brakesTorque > 0 ? "B: ON" : "B: OFF";
-        }
-
-        private void FixedUpdate()
-        {
-         thrustPercent = Mathf.Clamp(thrustPercent + FWInputs.Throttle, 0, 1); //This line sets thrustPercent according to the value of the Throttle input, limited to a value between 0 and 1.
-        
-
-        SetControlSurfecesAngles(FWInputs.Pitch, FWInputs.Roll, -FWInputs.Yaw, Flap);
-            aircraftPhysics.SetThrustPercent(thrustPercent);
-            foreach (var wheel in wheels)
-            {
-                wheel.brakeTorque = brakesTorque;
-                // small torque to wake up wheel collider
-                wheel.motorTorque = 0.01f;
-            }
-        }
-
-        public void SetControlSurfecesAngles(float pitch, float roll, float yaw, float flap)
-        {
-            foreach (var surface in controlSurfaces)
-            {
-                if (surface == null || !surface.IsControlSurface) continue;
-                switch (surface.InputType)
-                {
-                    case ControlInputType.Pitch:
-                        surface.SetFlapAngle(pitch * pitchControlSensitivity * surface.InputMultiplyer);
-                        break;
-                    case ControlInputType.Roll:
-                        surface.SetFlapAngle(roll * rollControlSensitivity * surface.InputMultiplyer);
-                        break;
-                    case ControlInputType.Yaw:
-                        surface.SetFlapAngle(yaw * yawControlSensitivity * surface.InputMultiplyer);
-                        break;
-                    case ControlInputType.Flap:
-                        surface.SetFlapAngle(Flap * surface.InputMultiplyer);
-                        break;
-                }
-            }
-        }
-
-        /*private void OnDrawGizmos()
-        {
-            if (!Application.isPlaying)
-                SetControlSurfecesAngles(FWInputs.Pitch, FWInputs.Roll, FWInputs.Yaw, Flap);
-        }*/
+        aircraftPhysics = GetComponent<AircraftPhysics>();
+        rb = GetComponent<Rigidbody>();
+        FWInputs = GetComponent<FixedWing_Inputs>();
     }
+
+    private void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            thrustPercent = FWInputs.Throttle;
+            //thrustPercent = thrustPercent > 0 ? 0 : 1f;
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Flap = Flap > 0 ? 0 : 0.3f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            brakesTorque = brakesTorque > 0 ? 0 : 100f;
+        }
+
+        displayText.text = "V: " + ((int)rb.velocity.magnitude).ToString("D3") + " m/s\n";
+        displayText.text += "A: " + ((int)transform.position.y).ToString("D4") + " m\n";
+        displayText.text += "T: " + (int)(thrustPercent * 100) + "%\n";
+        displayText.text += brakesTorque > 0 ? "B: ON" : "B: OFF";
+
+        propeller.transform.Rotate(0f, 0f, thrustPercent*propellerSpeed* Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        thrustPercent = Mathf.Clamp(thrustPercent + FWInputs.Throttle, 0, 1); //This line sets thrustPercent according to the value of the Throttle input, limited to a value between 0 and 1.
+
+
+        SetControlSurfacesAngles(FWInputs.Pitch, FWInputs.Roll, -FWInputs.Yaw, Flap);
+        aircraftPhysics.SetThrustPercent(thrustPercent);
+        foreach (var wheel in wheels)
+        {
+            wheel.brakeTorque = brakesTorque;
+            // small torque to wake up wheel collider
+            wheel.motorTorque = 0.01f;
+        }
+    }
+
+    public void SetControlSurfacesAngles(float pitch, float roll, float yaw, float flap)
+    {
+        foreach (var surface in controlSurfaces)
+        {
+            if (surface == null || !surface.IsControlSurface) continue;
+            switch (surface.InputType)
+            {
+                case ControlInputType.Pitch:
+                    surface.SetFlapAngle(pitch * pitchControlSensitivity * surface.InputMultiplyer);
+                    break;
+                case ControlInputType.Roll:
+                    surface.SetFlapAngle(roll * rollControlSensitivity * surface.InputMultiplyer);
+                    break;
+                case ControlInputType.Yaw:
+                    surface.SetFlapAngle(yaw * yawControlSensitivity * surface.InputMultiplyer);
+                    break;
+                case ControlInputType.Flap:
+                    surface.SetFlapAngle(Flap * surface.InputMultiplyer);
+                    break;
+            }
+        }
+    }
+
+    /*private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+            SetControlSurfecesAngles(FWInputs.Pitch, FWInputs.Roll, FWInputs.Yaw, Flap);
+    }*/
+}
 
 
--- Assets/Marius/Scripts/FixedWingScripts/FixedWing_Inputs.cs
+++ Assets/Marius/Scripts/FixedWingScripts/FixedWing_Inputs.cs
@@ -6,36 +6,26 @@ using UnityEngine.InputSystem;
 
     [RequireComponent(typeof(PlayerInput))] //Checks if a PlayerInput component is attached to the gameobject this script is assigned to, and if not makes one
 
-    public class FixedWing_Inputs : MonoBehaviour
-    {
-        #region Variables
+public class FixedWing_Inputs : MonoBehaviour
+{
+    #region Variables
 
-        private Vector2 _cyclic; //Corresponds to the cyclic action in our Drone action map. This is a Vector2 because the cyclic action is set to a Vector2.
+      
     private float _pitch;
     private float _roll;
-        private float _yaw; //Corresponds to the pedals action in our action map, is a float value because our pedals value goes from -1 to 1.
-        
+    private float _yaw; //Corresponds to the yaw action in our action map, is a float value because our pedals value goes from -1 to 1.
     private float _throttle; //Correspond to the throttle action in our action map, is a float value because our pedals value goes from -1 to 1.
 
-        //Below lines are encapsulating properties. Not entirely sure of that concept yet... Search "Encapsulating property" to find out
-        public Vector2  Cyclic { get => _cyclic; }
-
+    //Below lines are encapsulating properties. Not entirely sure of that concept yet... Search "Encapsulating property" to find out
     public float Pitch { get => _pitch; }
-
     public float Roll { get => _roll; }
+    public float Yaw { get => _yaw; }
+    public float Throttle { get => _throttle; }
 
-        public float Yaw { get => _yaw; }
-        public float Throttle { get => _throttle; }
+    #endregion
 
-        #endregion
-
-        //The input methods below will listen for messages sent by the player input - see PLayerInput component under "Behavior". 
-        #region Input Methods 
-        private void OnCyclic(InputValue value)
-        {
-            _cyclic = value.Get<Vector2>();
-        }
-
+    //The input methods below will listen for messages sent by the player input - see PLayerInput component under "Behavior". 
+    #region Input Methods
     private void OnPitch(InputValue value)
     {
         _pitch = value.Get<float>();
@@ -46,16 +36,16 @@ using UnityEngine.InputSystem;
         _roll = value.Get<float>();
     }
 
-        private void OnYaw(InputValue value)
-        {
-            _yaw = value.Get<float>();
-        }
-
-        public void OnThrottle(InputValue value)
-        {
-            _throttle = value.Get<float>() * 0.01f; //multiplied by 0.01f to calibrate the throttle so it is changed more softly
-        }
-        #endregion
+    private void OnYaw(InputValue value)
+    {
+        _yaw = value.Get<float>();
     }
+
+    public void OnThrottle(InputValue value)
+    {
+        _throttle = value.Get<float>() * 0.01f; //multiplied by 0.01f to calibrate the throttle so it is changed more softly
+    }
+    #endregion
+}
 
 
