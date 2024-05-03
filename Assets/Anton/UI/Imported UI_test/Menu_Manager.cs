ed349c63d192fbab42e0ccc572c3e991d1fd8d5b
akaise21 <94837345+akaise21@users.noreply.github.com>
2024-05-03 15:20:44 +0200

    MissionEndScreen

--- Assets/Anton/UI/Imported UI_test/Menu_Manager.cs
+++ Assets/Anton/UI/Imported UI_test/Menu_Manager.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
-
+using System;
+using TMPro;
 
 public class Menu_Manager : MonoBehaviour
 {
     public GameObject _menu;
-    
+
+    [SerializeField] private GameObject endScreenCanvas;
+    [SerializeField] private GameObject gpsJamCanvas;
+    [SerializeField] private GameObject tabletConnLossCanvas;
+    [SerializeField] private GameObject brokenOnTakeoffEmergencyCanvas;
+    [SerializeField] private TMP_Text reportStatusText;
+
+    private EmergencyManager _emergencyManager;
+    private int _activeEmergency;
+
+    private CrashLandingDetection _crashLandingDetection;
+    private bool _droneCrashed;
+
     public Transform _head;
     public float _spawnDistance = 2;
     public InputActionProperty _showMenuButton;
 
     private Vector3 _relativePosition;
+
+    private void Start()
+    {
+        _emergencyManager = FindObjectOfType<EmergencyManager>();
+        _activeEmergency = _emergencyManager.currentEmergency;
+
+        _emergencyManager = FindObjectOfType<CrashLandingDetection>();
+        _droneCrashed = CrashLandingDetection.droneCrashed;
+    }
 
     void Update()
     {
@@ -72,13 +94,42 @@ public class Menu_Manager : MonoBehaviour
         Debug.Log("QUIT Game!");
     }
 
-    public void EmergencySuccess()
+    public void MissionEndScreen()
     {
         Debug.Log("Emergency success");
-    }
+        PauseTime();
 
-    public void EmergencyFail()
-    {
-        Debug.Log("Emergency fail");
+        if (_droneCrashed == true)
+        {
+            reportStatusText.text = "Scenario failed!";
+        }
+        else
+        {
+            reportStatusText.text = "Scenario successful!";
+        }
+
+        endScreenCanvas.SetActive(true);
+
+        switch (_activeEmergency)
+        {
+            case 1:
+                gpsJamCanvas.SetActive(gpsJamCanvas);
+                gpsJamCanvas.transform.position = _head.position + new Vector3(_head.forward.x, 0, _head.forward.z).normalized * _spawnDistance;
+                gpsJamCanvas.transform.LookAt(new Vector3(_head.position.x, _menu.transform.position.y, _head.position.z));
+                gpsJamCanvas.transform.forward *= -1;
+                break;
+            case 2:
+                tabletConnLossCanvas.SetActive(tabletConnLossCanvas);
+                tabletConnLossCanvas.transform.position = _head.position + new Vector3(_head.forward.x, 0, _head.forward.z).normalized * _spawnDistance;
+                tabletConnLossCanvas.transform.LookAt(new Vector3(_head.position.x, _menu.transform.position.y, _head.position.z));
+                tabletConnLossCanvas.transform.forward *= -1;
+                break;
+            case 3:
+                brokenOnTakeoffEmergencyCanvas.SetActive(brokenOnTakeoffEmergencyCanvas);
+                brokenOnTakeoffEmergencyCanvas.transform.position = _head.position + new Vector3(_head.forward.x, 0, _head.forward.z).normalized * _spawnDistance;
+                brokenOnTakeoffEmergencyCanvas.transform.LookAt(new Vector3(_head.position.x, _menu.transform.position.y, _head.position.z));
+                brokenOnTakeoffEmergencyCanvas.transform.forward *= -1;
+                break;
+        }
     }
 }
